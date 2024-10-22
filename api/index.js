f7f4d746c9890579377114f3e83b3590a34e5e1a
rashi458 <rashi3909@gmail.com>
2024-10-22 15:55:29 +0530

    Initial commit for Task1

---
+++ api/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route.js');
+const authRouter = require('./routes/auth.route.js');
+const listingRouter = require('./routes/listing.route.js');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+dotenv.config();
+
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const clientPath = path.join(__dirname, '..', 'client', 'dist');
+app.use(express.static(clientPath));
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
